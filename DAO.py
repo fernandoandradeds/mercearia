96994b20f1c2faa57dacf9c97bc094a2160e8279
Fernando Andrade <fasylva@hotmail.com>
2025-08-27 22:18:48 -0300

    conclusão da Dao

--- DAO.py
+++ DAO.py
@@ -43,5 +43,84 @@ class DaoVenda:
             vend.append(Venda(Produtos(i[0], i[1], i[2]), i[3], i[4], i[5], i[6]))
         return vend
 
-x = DaoVenda.ler()
-print(x[0].itensVendidos.nome)  # Teste de leitura do arquivo venda.txt
+class DaoEstoque:
+
+    @classmethod
+    def salvar(cls, produto: Produtos, quantidade):
+        with open('estoque.txt', 'a') as file:
+            file.writelines(produto.nome + '|' + produto.preco + '|' + produto.categoria + '|' + str(quantidade))
+            file.writelines('\n')
+
+    @classmethod
+    def ler(cls):
+        with open('estoque.txt', 'r') as file:
+            cls.estoque = file.readlines()
+
+        cls.estoque = list(map(lambda x: x.replace('\n', ''), cls.estoque))
+        cls.estoque = list(map(lambda x: x.split('|'), cls.estoque))
+        est = []
+        if len(cls.estoque) > 0:
+            for i in cls.estoque:
+                est.append(Estoque(Produtos(i[0], i[1], i[2]), i[3]))
+        return est
+
+class DaoFornecedor:
+
+    @classmethod
+    def salvar(cls, fornecedor: Fornecedor):
+        with open('fornecedores.txt', 'a') as file:
+            file.writelines(fornecedor.nome + '|' + fornecedor.cnpj + '|' + fornecedor.telefone + '|' + fornecedor.categoria)
+            file.writelines('\n')
+
+    @classmethod
+    def ler(cls):
+        with open('fornecedores.txt', 'r') as file:
+            cls.fornecedor = file.readlines()
+
+        cls.fornecedor = list(map(lambda x: x.replace('\n', ''), cls.fornecedor))
+        cls.fornecedor = list(map(lambda x: x.split('|'), cls.fornecedor))
+        forn = []
+        for i in cls.fornecedor:
+            forn.append(Fornecedor(i[0], i[1], i[2], i[3]))
+        return forn
+
+class DaoPessoa:
+
+    @classmethod
+    def salvar(cls, pessoa: Pessoa):
+        with open('clientes.txt', 'a') as file:
+            file.writelines(pessoa.nome + '|' + pessoa.telefone + '|' + pessoa.cpf + '|' + pessoa.email + '|' + pessoa.endereco)
+            file.writelines('\n')
+
+    @classmethod
+    def ler(cls):
+        with open('clientes.txt', 'r') as file:
+            cls.pessoa = file.readlines()
+
+        cls.pessoa = list(map(lambda x: x.replace('\n', ''), cls.pessoa))
+        cls.pessoa = list(map(lambda x: x.split('|'), cls.pessoa))
+        pes = []
+        for i in cls.pessoa:
+            pes.append(Pessoa(i[0], i[1], i[2], i[3], i[4]))
+        return pes
+
+class DaoFuncionario:
+
+    @classmethod
+    def salvar(cls, funcionario: Funcionario):
+        with open('funcionarios.txt', 'a') as file:
+            file.writelines(funcionario.clt + '|' + funcionario.nome + '|' + funcionario.telefone + '|' + funcionario.cpf + '|' +
+                            funcionario.email + '|' + funcionario.endereco + '|' + str(funcionario.salario) + '|' + funcionario.cargo)
+            file.writelines('\n')
+
+    @classmethod
+    def ler(cls):
+        with open('funcionarios.txt', 'r') as file:
+            cls.funcionario = file.readlines()
+
+        cls.funcionario = list(map(lambda x: x.replace('\n', ''), cls.funcionario))
+        cls.funcionario = list(map(lambda x: x.split('|'), cls.funcionario))
+        func = []
+        for i in cls.funcionario:
+            func.append(Funcionario(i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7]))
+        return func
